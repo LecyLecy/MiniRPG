9bb4bdda63d2a975060e38adb72288876941d6a6
LecyLecy <adinza11806@gmail.com>
2025-11-26 11:24:00 +0700

    Initialize

---
+++ MiniRPG/src/mainPackage/MainClass.java
@@ -0,0 +1,13 @@
+package mainPackage;
+
+import javax.swing.*;
+
+public class MainClass {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setTitle("JFrame title goes here");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(420,420);
+        frame.setVisible(true);
+    }
+}
